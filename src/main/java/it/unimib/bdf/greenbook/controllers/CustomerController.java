5a55731186f51fa6655b0c6ad080974521275a11
Alessandro Fornaro <fornaroale@outlook.com>
2021-12-28 00:11:29 +0100

    Add customers' allergens selection; fixed related RELP

--- src/main/java/it/unimib/bdf/greenbook/controllers/CustomerController.java
+++ src/main/java/it/unimib/bdf/greenbook/controllers/CustomerController.java
@@ -1,6 +1,8 @@
 package it.unimib.bdf.greenbook.controllers;
 
+import it.unimib.bdf.greenbook.models.Allergen;
 import it.unimib.bdf.greenbook.models.Customer;
+import it.unimib.bdf.greenbook.services.AllergenService;
 import it.unimib.bdf.greenbook.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class CustomerController {
 
     @Autowired
     private CustomerService service;
+
+    @Autowired
+    private AllergenService allergenService;
+
 
     @GetMapping("/customers")
     public String showAllCustomers(Model model) {
@@ -29,12 +36,18 @@ public class CustomerController {
     @GetMapping("/new-customer")
     public String showNewCustomerForm(Model model) {
         model.addAttribute("customer", new Customer());
+
+        // Retrieve persisted allergens list
+        List<Allergen> persistedAllergens = allergenService.findAll();
+        model.addAttribute("allergensList", persistedAllergens);
+
         return "customer/new-customer";
     }
 
     @PostMapping(value = "/addCustomer", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String addNewCustomer(@Valid @ModelAttribute Customer customer, BindingResult result, Model model) {
         if (result.hasErrors()) {
+            model.addAttribute("allergensList", getPersistedAllergens());
             return "customer/new-customer";
         }
         service.save(customer);
@@ -46,13 +59,17 @@ public class CustomerController {
     public String showCustomerById(@PathVariable Long id, Model model) {
         Customer customer = service.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid customer Id:" + id));
+
         model.addAttribute("customer", customer);
+        model.addAttribute("allergensList", getPersistedAllergens());
+
         return "customer/edit-customer";
     }
 
     @PostMapping("/updateCustomer/{id}")
     public String updateCustomer(@PathVariable Long id, @Valid @ModelAttribute Customer customer, BindingResult result, Model model) {
         if (result.hasErrors()) {
+            model.addAttribute("allergensList", getPersistedAllergens());
             return "customer/edit-customer";
         }
         service.findById(id)
@@ -69,6 +86,11 @@ public class CustomerController {
         service.deleteById(id);
         model.addAttribute("customers", service.findAll());
         return "customer/customers";
+    }
+
+    private List<Allergen> getPersistedAllergens(){
+        // Retrieve persisted allergens list
+        return allergenService.findAll();
     }
 
 }
--- src/main/java/it/unimib/bdf/greenbook/models/Allergen.java
+++ src/main/java/it/unimib/bdf/greenbook/models/Allergen.java
@@ -2,6 +2,7 @@ package it.unimib.bdf.greenbook.models;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
 @Entity
 public class Allergen {
@@ -12,6 +13,25 @@ public class Allergen {
 	@Basic(optional = false)
 	@NotEmpty(message = "Il nome dell'allergene non puo' essere lasciato vuoto")
 	private String name;
+
+	@ManyToMany(mappedBy = "allergies")
+	Set<Customer> allergicCustomer;
+
+	@Override
+	public int hashCode() {
+		return Long.valueOf(getId()).hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (! (obj instanceof Allergen)) {
+			return false;
+		}
+		return this.getId() == ((Allergen)obj).getId();
+	}
 
 	public long getId() {
 		return id;
@@ -28,4 +48,9 @@ public class Allergen {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String toString(){
+		return getName();
+	}
+
 }
